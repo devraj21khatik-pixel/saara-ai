65a62de05daecee1ddc75468786847b26fe08c5a
devraj21khatik-pixel <devraj21khatik@gmail.com>
2026-08-13 00:00:23 +0530

    Set custom template folder for Flask app

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 import re
 import requests
 from flask import Flask, render_template, request, jsonify, session
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+
 app.secret_key = "saara_secret_key_123"
 
 # Environment Variable se read karega (GitHub par koi error nahi aayega)
